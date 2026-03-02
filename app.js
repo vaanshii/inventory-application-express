798d49679f8858ae772687891c72258b615ac1df
vaanshii <155531131+vaanshii@users.noreply.github.com>
2026-03-02 15:26:17 -0800

    Initialize view engine.

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 
 const PORT = process.env.SERVER_PORT;
 
+app.set("views", path.join(__dirname), "views");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
