40d071dc06221e484f03ba04ef597060a302146c
vaanshii <155531131+vaanshii@users.noreply.github.com>
2026-03-12 10:01:00 -0700

    Validator for adding new firearm.

---
+++ validators/firearmValidator.js
@@ -0,0 +1,67 @@
+const { body } = require("express-validator");
+
+const lengthErrorMsg = (number) =>
+	`must be between 1 and ${number} characters.`;
+
+const emptyValue = "cannot be empty.";
+
+const validateFirearm = [
+	body("modelName")
+		.trim()
+		.notEmpty()
+		.withMessage(`Model Name ${emptyValue}`)
+		.bail()
+		.isLength({ min: 1, max: 50 })
+		.withMessage(`Model name ${lengthErrorMsg(50)}`),
+	body("serialNumber")
+		.trim()
+		.notEmpty()
+		.withMessage(`Serial Number ${emptyValue}`)
+		.bail()
+		.isLength({ min: 1, max: 20 })
+		.withMessage(`Serial Number ${lengthErrorMsg(20)}`),
+	body("ammoType")
+		.trim()
+		.notEmpty()
+		.withMessage(`Ammo Type ${emptyValue}`)
+		.bail()
+		.isLength({ min: 1, max: 20 })
+		.withMessage(`Ammo type ${lengthErrorMsg(20)}`),
+	body("caliberName")
+		.trim()
+		.notEmpty()
+		.withMessage(`Caliber Name ${emptyValue}`)
+		.bail()
+		.isLength({ min: 1, max: 30 })
+		.withMessage(`Caliber Name ${lengthErrorMsg(30)}`),
+	body("manufacturerName")
+		.trim()
+		.notEmpty()
+		.withMessage(`Manufacturer Name ${emptyValue}`)
+		.bail()
+		.isLength({ min: 1, max: 30 })
+		.withMessage(`Manufacturer Name ${lengthErrorMsg(30)}`),
+	body("country")
+		.trim()
+		.notEmpty()
+		.withMessage(`Country ${emptyValue}`)
+		.bail()
+		.isAlpha("en-US", { ignore: " " })
+		.withMessage("Country must only contain letters.")
+		.isLength({ min: 1, max: 30 })
+		.withMessage(`Country name ${lengthErrorMsg(30)}`),
+	body("purchasePrice")
+		.trim()
+		.optional({ values: "falsy" })
+		.isNumeric()
+		.withMessage("Purchase price must be a number.")
+		.isLength({ min: 1, max: 20 })
+		.withMessage(`Purchase price ${lengthErrorMsg(20)}`),
+	body("imagePath")
+		.trim()
+		.optional({ values: "falsy" })
+		.isURL()
+		.withMessage("Image Link must be a valid link."),
+];
+
+module.exports = validateFirearm;
