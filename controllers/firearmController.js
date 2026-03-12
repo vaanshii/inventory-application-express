63e77f17ad14c27fa5ad16540e4d9154d6314d76
vaanshii <155531131+vaanshii@users.noreply.github.com>
2026-03-12 10:02:25 -0700

    Implements post request and get all request.

--- controllers/firearmController.js
+++ controllers/firearmController.js
@@ -1,7 +1,15 @@
-exports.listFirearmsGet = (req, res) => {
-	// query here to get all firearms
+const { matchedData, validationResult } = require("express-validator");
+const validateFirearm = require("../validators/firearmValidator");
+const Firearm = require("../models/Firearm");
+
+exports.listFirearmsGet = async (req, res) => {
+	const firearmData = await Firearm.getAll();
+	// * logging
+	// console.log(firearmData);
+
 	res.render("views/index", {
 		title: "ArmaVault | Firearms",
+		firearms: firearmData,
 	});
 };
 
@@ -11,5 +19,24 @@ exports.showFirearmGet = (req, res) => {
 };
 
 exports.addFirearmGet = (req, res) => {
-	res.render("views/addFirearm", { title: "Add Firearm" });
+	res.render("views/addFirearm", { title: "Add Firearm", formData: "" });
 };
+
+exports.addFirearmPost = [
+	validateFirearm,
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).render("views/addFirearm", {
+				title: "Add Firearm",
+				errors: errors.array(),
+				formData: req.body,
+			});
+		}
+		const gunData = matchedData(req);
+		console.log(gunData);
+
+		Firearm.createWithCheck(gunData);
+		res.redirect("/");
+	},
+];
