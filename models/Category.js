b03623a4229de2679278c0faab522ff99c5d5bbf
vaanshii <155531131+vaanshii@users.noreply.github.com>
2026-03-13 16:52:47 -0700

    Model for Category objects.

---
+++ models/Category.js
@@ -0,0 +1,23 @@
+const pool = require("../db/pool");
+
+class Category {
+	constructor({ id, name }) {
+		((this.id = id), (this.name = name));
+	}
+
+	static async getCategories() {
+		const query = `
+        SELECT * FROM Category ORDER BY Name ASC;
+        `;
+
+		try {
+			const { rows } = await pool.query(query);
+			return rows;
+		} catch (error) {
+			console.log("[getCategories]Query Error: ", error);
+			throw error;
+		}
+	}
+}
+
+module.exports = Category;
