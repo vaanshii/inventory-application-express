16cd5e56e5b9de8e119107b738f74fc6436be19d
vaanshii <155531131+vaanshii@users.noreply.github.com>
2026-03-13 16:49:42 -0700

    Fix bug where after adding new firearm, the new data is not rendered yet.

--- controllers/firearmController.js
+++ controllers/firearmController.js
@@ -1,9 +1,13 @@
 const { matchedData, validationResult } = require("express-validator");
 const validateFirearm = require("../validators/firearmValidator");
+
 const Firearm = require("../models/Firearm");
+const Category = require("../models/Category");
+const getCurrentDate = require("../utils/getCurrentDate");
 
 exports.listFirearmsGet = async (req, res) => {
 	const firearmData = await Firearm.getAll();
+
 	// * logging
 	// console.log(firearmData);
 
@@ -18,25 +22,49 @@ exports.showFirearmGet = (req, res) => {
 	res.render("views/showFirearm", { title: "Gun Name" });
 };
 
-exports.addFirearmGet = (req, res) => {
-	res.render("views/addFirearm", { title: "Add Firearm", formData: "" });
+exports.addFirearmGet = async (req, res) => {
+	const today = getCurrentDate;
+	const categories = await Category.getCategories();
+	console.log(categories);
+
+	res.render("views/addFirearm", {
+		title: "Add Firearm",
+		formData: "",
+		maxDate: today,
+		categories: categories,
+	});
 };
 
 exports.addFirearmPost = [
 	validateFirearm,
 	async (req, res) => {
+		const categories = await Category.getCategories();
+		const today = getCurrentDate;
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).render("views/addFirearm", {
 				title: "Add Firearm",
 				errors: errors.array(),
+				maxDate: today,
 				formData: req.body,
+				categories: categories,
 			});
 		}
 		const gunData = matchedData(req);
 		console.log(gunData);
-
-		Firearm.createWithCheck(gunData);
-		res.redirect("/");
+		try {
+			await Firearm.createWithCheck(gunData);
+			res.redirect("/");
+		} catch (error) {
+			if (error.code === "23505") {
+				return res.status(400).render("views/addFirearm", {
+					title: "Add Firearm",
+					errors: [{ msg: "Serial number already exists." }],
+					maxDate: today,
+					formData: req.body,
+					categories: categories,
+				});
+			}
+		}
 	},
 ];
